62f9c6d982e90a0be1e24121dbd734201da43a54
Anna Török <torokanna10@gmail.com>
2026-04-09 12:23:40 +0200

    Initial commit

---
+++ shared/index.ts
@@ -0,0 +1,24 @@
+export interface User {
+    id: number;
+    username: string;
+    email: string;
+    avatar_url: string;
+    is_online: string;
+}
+
+export interface Message {
+    id: number;
+    room_id: number;
+    sender_id: number;
+    content: string;
+    created_at: string;
+}
+
+export type RoomType = 'public' | 'private' | 'pw_protected' | 'dm';
+
+export interface Room {
+    id: number,
+    name: string;
+    type: string;
+    owner_id: number;
+}
